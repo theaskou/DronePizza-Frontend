4187a293696b797b7353e64a909be57f169cfc18
thea <thsk0001@stud.kea.dk>
2025-01-17 02:42:38 +0100

    Opgave 3

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ async function fetchDeliveries() {
     const headerBarHTML = `<div class="container-fluid">
         <div>Sidst opdateret: ${hours}:${minutes}:${seconds}</div>
         <button id="addDroneButton">Opret drone</button>
+        <button id="simulateNewOrderButton" class="secondary">Simuler bestilling</button>
     </div>`
 
     const tableHead = `
@@ -30,6 +31,7 @@ async function fetchDeliveries() {
                 <th>Adresse</th>
                 <th>Pizza</th>
                 <th>Pris</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
@@ -37,16 +39,17 @@ async function fetchDeliveries() {
 
     let tableBody = "";
     deliveries.forEach((delivery) => {
-        const addDroneButtonHTML = `<button id="${delivery.deliveryID}">Tilføj drone</button>`
+        const addDroneButtonHTML = `<button id="${delivery.deliveryID}" class="add-drone-button">Tilføj drone</button>`
 
         tableBody += `
             <tr>
                 <td>${delivery.deliveryID}</td>
-                <td>${delivery.expectedDeliveryTime}</td>
+                <td>${delivery.expectedDeliveryTime.slice(0, 5)}</td>
                 <td>${delivery.drone ? delivery.drone.droneID : addDroneButtonHTML}</td>
                 <td>${delivery.deliveryAddress}</td>
                 <td>${delivery.pizza.pizzaName}</td>
                 <td>${delivery.pizza.pizzaPrice}</td>
+                <td><button id="${delivery.deliveryID}" class="simulate-delivery-button secondary">Simuler levering</button></td>
             </tr>
         `
     })
@@ -62,23 +65,49 @@ async function fetchDeliveries() {
 
     appElement.querySelector("#addDroneButton").addEventListener("click", () => addNewDroneHandler());
 
-    // Add eventListeners to buttons calling addDroneHandler with id
-    const buttons = appElement.querySelectorAll("tbody > tr > td > button");
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", () => addDroneHandler(buttons[i].id));
+    appElement.querySelector("#simulateNewOrderButton").addEventListener("click", () => simulateNewOrder());
+
+    const addDroneButtons = appElement.querySelectorAll("tbody > tr > td > button.add-drone-button");
+    for (let i = 0; i < addDroneButtons.length; i++) {
+        addDroneButtons[i].addEventListener("click", () => addDroneHandler(addDroneButtons[i].id));
+    }
+
+    const simulateDeliveryButtons = appElement.querySelectorAll("tbody > tr > td > button.simulate-delivery-button");
+    for (let i = 0; i < simulateDeliveryButtons.length; i++) {
+        simulateDeliveryButtons[i].addEventListener("click", () => simulateDeliveryHandler(simulateDeliveryButtons[i].id))
     }
 }
 
-async function addDroneHandler(deliveryID){
+async function addDroneHandler(deliveryID) {
     fetch(`http://localhost:8080/deliveries/schedule/${deliveryID}`, {method: "POST"}).then(() => fetchDeliveries());
 }
 
-function addNewDroneHandler(){
+function simulateNewOrder() {
+    const newOrder = {
+        "deliveryAddress": "Delivery Address #2341",
+        "pizza": {
+            "pizzaID": 1,
+            "pizzaName": "Margherita",
+            "pizzaPrice": 65
+        }
+    }
+    fetch(`http://localhost:8080/deliveries/add`, {
+        method: "POST", headers: {
+            'Content-Type': 'application/json',
+        }, body: JSON.stringify(newOrder)
+    }).then(() => fetchDeliveries());
+}
+
+function addNewDroneHandler() {
     fetch(`http://localhost:8080/drones/add`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({}), // Include the request payload if required
-    }).then((response) => response.json()).then((data)=> window.alert(`Drone oprettet ✅ (ID: ${data.droneID} · Status: ${data.operationalStatus} · UUID: ${data.serialUUID} · Station: ${data.station.stationID})`));
+    }).then((response) => response.json()).then((data) => window.alert(`Drone oprettet ✅ (ID: ${data.droneID} · Status: ${data.operationalStatus} · UUID: ${data.serialUUID} · Station: ${data.station.stationID})`));
+}
+
+async function simulateDeliveryHandler(deliveryID) {
+    fetch(`http://localhost:8080/deliveries/finish/${deliveryID}`, {method: "POST"}).then(() => fetchDeliveries());
 }
